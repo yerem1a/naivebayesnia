aa2dcdf95649687e981037ce0da61dbb81718c4a
yeremia purba <yeremiapurba14@gmail.com>
2023-08-29 09:28:15 +0700

    masukan semua

---
+++ database/migrations/2023_08_27_044524_create_penyakit_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePenyakitTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('penyakit', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode', 2)->unique();
+            $table->string('nama');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('penyakit');
+    }
+}
---
+++ database/migrations/2023_08_27_044541_create_gejala_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGejalaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gejala', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode', 3)->unique();
+            $table->text('gejala');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gejala');
+    }
+}
